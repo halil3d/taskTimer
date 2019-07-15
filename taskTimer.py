99352698ab7f7bd739433d194b35170ba6430490
Halil Mehmet <halil3d@gmail.com>
2019-07-15 23:47:55 +1000

    Added listwidget

--- taskTimer.py
+++ taskTimer.py
@@ -12,86 +12,119 @@ import utils
 class TaskTimer(QtGui.QWidget):
 
     def __init__(self, *arg, **kwarg):
-        super(TaskTimer, self).__init__(*arg, **kwarg)
+        super(self.__class__, self).__init__(*arg, **kwarg)
+        self.listWidget = QtGui.QListWidget(self)
+        self.toggleNewTimer = QtGui.QPushButton("+")
         self.setupUI()
 
     def setupUI(self):
         self.mainLayout = QtGui.QVBoxLayout()
+        self.setLayout(self.mainLayout)
 
         x, y, w, h = 100, 100, 350, 50
         self.setGeometry(x, y, w, h)
 
-        self.lcdDisplay = TimerWidget(self)
-        self.mainLayout.addWidget(self.lcdDisplay)
-        self.setLayout(self.mainLayout)
+        self.mainLayout.addWidget(self.listWidget)
+        self.mainLayout.addWidget(self.toggleNewTimer)
+
+        self.toggleNewTimer.clicked.connect(self.addNewTask)
+
+    def addNewTask(self):
+        for i in xrange(self.listWidget.count()):
+            item = self.listWidget.item(i)
+            widget = self.listWidget.itemWidget(item)
+            widget.stop()
+
+        item = QtGui.QListWidgetItem(self.listWidget)
+        item.setSizeHint(QtCore.QSize(100, 50))
+        newTask = TaskWidget()
+        newTask.start()
+        self.listWidget.setItemWidget(item, newTask)
+        self.listWidget.addItem(item)
 
 
 class TaskWidget(QtGui.QWidget):
-    pass
-
-
-class TimerWidget(QtGui.QWidget):
-
-    def __init__(self, parent=None, displayButtons=True, *args, **kwargs):
-        super(self.__class__, self).__init__(*args, **kwargs)
-        self.displayButtons = displayButtons
-        self.timerID = None
-        self.elapsed = None
-
-        self.timer = QtCore.QElapsedTimer()
-
+    def __init__(self, *arg, **kwarg):
+        super(self.__class__, self).__init__(*arg, **kwarg)
+        self.taskLineEdit = QtGui.QLineEdit(self)
+        self.timerWidget = TimerWidget(self)
+        self.stopButton = QtGui.QPushButton("S")
+        self.resetButton = QtGui.QPushButton("R")
         self.setupUI()
-        self.resetTimer()
 
     def setupUI(self):
-        self.mainlayout = QtGui.QHBoxLayout()
-        self.setLayout(self.mainlayout)
+        self.mainLayout = QtGui.QHBoxLayout()
+        self.setLayout(self.mainLayout)
 
-        self.lcdDisplay = QtGui.QLCDNumber(self)
-        self.lcdDisplay.setDigitCount(10)
-        self.mainlayout.addWidget(self.lcdDisplay)
+        self.mainLayout.addWidget(self.taskLineEdit)
+        self.mainLayout.addWidget(self.timerWidget)
         self.buttonLayout = QtGui.QHBoxLayout()
+        self.mainLayout.addLayout(self.buttonLayout)
 
-        if self.displayButtons:
-            self.toggleButton = QtGui.QPushButton("S/S")
-            self.resetButton = QtGui.QPushButton("R")
-            self.buttonLayout.addWidget(self.toggleButton)
-            self.buttonLayout.addWidget(self.resetButton)
+        self.stopButton.setFixedSize(24, 24)
+        self.resetButton.setFixedSize(24, 24)
+        self.buttonLayout.addWidget(self.stopButton)
+        self.buttonLayout.addWidget(self.resetButton)
 
-            self.toggleButton.clicked.connect(self.toggleTimer)
-            self.resetButton.clicked.connect(self.resetTimer)
+        self.stopButton.clicked.connect(self.stop)
+        self.resetButton.clicked.connect(self.reset)
 
-        self.mainlayout.addLayout(self.buttonLayout)
+    def start(self):
+        self.timerWidget.start()
 
-    def startTimer(self):
+    def stop(self):
+        self.timerWidget.stop()
+
+    def reset(self):
+        self.timerWidget.reset()
+
+    def toggle(self):
+        self.timerWidget.toggle()
+
+
+class TimerWidget(QtGui.QLCDNumber):
+
+    def __init__(self, parent=None, *args, **kwargs):
+        super(self.__class__, self).__init__(*args, **kwargs)
+        self.timerID = None
+        self.elapsed = None
+        self.timer = QtCore.QElapsedTimer()
+
+        self.setDigitCount(8)
+        self.setFixedHeight(24)
+        self.setFixedWidth(120)
+
+        self.reset()
+
+    def start(self):
         if not self.timerID:
-            self.timerID = super(self.__class__, self).startTimer(1)
+            self.timerID = self.startTimer(1)
         self.timer.start()
 
-    def stopTimer(self):
+    def stop(self):
         if self.timerID:
             self.elapsed += self.timer.elapsed()
             self.killTimer(self.timerID)
             self.timerID = None
 
-    def resetTimer(self):
+    def reset(self):
         if self.timerID:
             self.killTimer(self.timerID)
             self.timerID = None
 
         self.elapsed = 0
-        self.lcdDisplay.display("00:00:00")
+        self.display("00:00:00")
 
-    def toggleTimer(self):
+    def toggle(self):
         if self.timerID:
-            self.stopTimer()
+            self.stop()
         else:
-            self.startTimer()
+            self.start()
 
     def timerEvent(self, event):
         milliseconds = self.elapsed + self.timer.elapsed()
         timestring = milli2LCDString(milliseconds)
-        self.lcdDisplay.display(timestring)
+        self.display(timestring)
 
 
 def milli2LCDString(milliseconds):
